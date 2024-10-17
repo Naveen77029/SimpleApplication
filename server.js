0c85b467501c1603ca643940d1f08152c5ba11d0
Naveen <naveen.peruganti@covalenseglobal.com>
2024-10-17 19:52:50 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Serve the frontend file
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
